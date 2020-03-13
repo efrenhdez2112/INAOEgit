216af9fff8a712e2f4eeaff6991ad95279fba8c5
Efren_Hernandez <efrenhdez9812@hotmail.com>
2020-03-12 22:56:32 -0600

    Intento 1

---
+++ ki.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <complex>
+
+using namespace std;
+int main()
+{
+		long double q=1.602176634e-19;
+	long double m_e=9.1093837015e-31;
+	long double hbar=1.0545718176461565e-34;
+	extern int i;
+	extern long double Eq;
+	cout<<i;
+
+return 0;
+}
