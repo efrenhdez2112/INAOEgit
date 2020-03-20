9d34d3577f0e76f754606eb09d0862aad0f20ed9
Efren_Hernandez <efrenhdez9812@hotmail.com>
2020-03-19 18:42:04 -0600

    Libreria de matrices, propagate

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <math.h>
 #include <vector>
 #include <complex>
+#include <Eigen/Dense> //Libreria usada para la multiplicacion de matrices
 
+using namespace Eigen;
 using namespace std;
 
 int main ()
@@ -11,6 +13,7 @@ int main ()
 	long double m_e=9.1093837015e-31;
 	long double hbar=1.0545718176461565e-34;
 	long double nm=1e-9;
+	complex <long double> im ( 0 , 1 );	
 	int i;
 	cout<<"Ecuacion de Schrodinger."<<endl;
 	cout<<"Ingrese el numero de potenciales a analizar: ";
@@ -47,12 +50,12 @@ int main ()
 		cin>>z[j];
 		zetas[j]=z[j]*nm;
 	}
+	
 complex <long double> k[i];
 complex <long double> kt[i];
 complex <long double> res1;
 complex <long double> res2;
-complex <long double> im ( 0 , 1 );
-		
+
 		for(int j=0; j<i; j++)
 {
     if (Eq>V[j])
@@ -70,19 +73,23 @@ complex <long double> im ( 0 , 1 );
     cout<<"< V= "<<V[j]<<" , "<<"m_eff = "<<me[j]<<" , "<<"E = "<<Eq<<" >"<<endl;
         cout<<"k"<<j+1<<"= "<<k[j]<<"\n";
         cout<<"kt"<<j+1<<"= "<<kt[j]<<"\n\n";
-
 }
+
 complex <long double> T11;
 complex <long double> T12;
 complex <long double> T21;
 complex <long double> T22; 
-complex <long double> mt[2][2]={{T11,T12},{T21,T22}};
-complex <long double> mr[2][2]={{(1,1),(1,1)},{(1,1),(1,1)}};
+Matrix <complex<long double>,2,2> mt;
+Matrix <complex<long double>,2,2> mr;
+mr(0,0)=(1,1);
+mr(1,0)=(1,1);
+mr(0,1)=(1,1);
+mr(1,1)=(1,1);
 complex <long double> two (2 , 0);
 complex <long double> ex;
 complex <long double> exp_p;
 complex <long double> exp_n;
-complex <long double> T_11, T_12, T_21, T_22;
+
 for (int j=0; j<i-1; j++)
 {
 	ex = im*k[j]*(zetas[j+1]-zetas[j]);
@@ -92,30 +99,15 @@ for (int j=0; j<i-1; j++)
 	T12 = ((kt[j+1]-kt[j])/(two*kt[j+1]))*exp_n;
 	T21 = ((kt[j+1]-kt[j])/(two*kt[j+1]))*exp_p;
 	T22 = ((kt[j+1]+kt[j])/(two*kt[j+1]))*exp_n;
-	mt[0][0]=T11;
-	mt[0][1]=T12;
-	mt[1][0]=T21;
-	mt[1][1]=T22;
-	cout<<"Matriz "<<j+1<<" = "<<endl;
-	cout<<mt[0][0]<<" , "<<mt[0][1]<<"\n";
-	cout<<mt[1][0]<<" , "<<mt[1][1]<<"\n\n";
-	T_11 =mr[0][0]*mt[0][0]+mr[0][1]*mt[1][0];
-//	cout<<"T11 = "<<T_11;
-	T_12 =mr[0][0]*mt[0][1]+mr[0][1]*mt[1][1];
-//	cout<<"T12 = "<<T_12;
-	T_21 =mr[1][0]*mt[0][0]+mr[1][1]*mt[1][0];
-//	cout<<"T21 = "<<T_21;
-	T_22 =mr[1][0]*mt[0][1]+mr[1][1]*mt[1][1];
-//	cout<<"T22 = "<<T_22;
-	T_11 = mr[0][0];
-	cout<<endl<<"mr 0 0"<<mr[0][0];
-	T_12 = mr[0][1];
-	T_21 = mr[1][0];
-	T_22 = mr[1][1];
+    mt(0,0)=T11;
+    mt(1,0)=T21;
+    mt(0,1)=T12;
+    mt(1,1)=T22;
+	cout<<"\nMatriz "<<j+1<<" = "<<endl<<endl;
+	cout<<mt;
+	mr = mr*mt;
 }
-cout<<"´T´ total = "<<endl;
-    cout<<mr[0][0]<<" , "<<mr[0][1]<<"\n";
-	cout<<mr[1][0]<<" , "<<mr[1][1]<<"\n\n";
+cout<<"\n´T´ total = "<<endl<<mr<<endl;
     return 0;
 }
 
